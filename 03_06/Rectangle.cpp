7ce776f4a0456510cd0c9b7145174f2121532b9d
tominagayuuki <k021g1421@g.neec.ac.jp>
2023-01-31 15:02:00 +0900

    評価課題

---
+++ 03_06/Rectangle.cpp
@@ -0,0 +1,12 @@
+#include "Rectangle.h"
+#include<stdio.h>
+
+void Rectangle::size()
+{
+	printf("‹éŒ`‚Ì–ÊÏ‚ğ‹‚ß‚é\n");
+}
+
+void Rectangle::draw()
+{
+	printf(" \n");
+}
---
+++ 03_06/Rectangle.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"IShape.h"
+
+class Rectangle : public IShape
+{
+public:
+	void size() override;
+	void draw() override;
+};
+
